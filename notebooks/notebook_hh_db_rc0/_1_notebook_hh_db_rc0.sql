652e8c8db1200cb8a5375c1d92444ccd87ba4b51
paolomococci <paolomococci@gmail.com>
2023-03-28 09:19:29 +0200

    modified:   notebooks/notebook_hh_db_rc0/_1_notebook_hh_db_rc0.sql

--- notebooks/notebook_hh_db_rc0/_1_notebook_hh_db_rc0.sql
+++ notebooks/notebook_hh_db_rc0/_1_notebook_hh_db_rc0.sql
@@ -5,3 +5,20 @@
 /*markdown
 ## backend database administration
 */
+
+USE `notebook_db_rc0`;
+
+/*markdown
+## Show databases
+*/
+
+SHOW DATABASES;
+
+/*markdown
+## To create a new database and assign its privileges
+*/
+
+CREATE DATABASE notebook_hh_db_rc0;
+GRANT ALL PRIVILEGES ON notebook_hh_db_rc0.* TO 'username'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'username'@'localhost';
