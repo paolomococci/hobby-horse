c033fc04f689d792e205f03d601b71b2ea9f9e19
paolomococci <paolomococci@gmail.com>
2023-04-08 22:54:39 +0200

    modified:   notebooks/dressage_rc0/create_database.sql

--- notebooks/dressage_rc0/create_database.sql
+++ notebooks/dressage_rc0/create_database.sql
@@ -3,3 +3,12 @@
 */
 
 CREATE DATABASE `dressage_rc0`;
+
+/*markdown
+## assign privileges to a specific user
+*/
+
+CREATE DATABASE notebook_hh_db_rc0;
+GRANT ALL PRIVILEGES ON notebook_hh_db_rc0.* TO 'username'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'username'@'localhost';
