afd21190e52b24e5d3b478c0588c87cc6e4e7595
paolomococci <paolomococci@gmail.com>
2023-04-08 23:03:08 +0200

    modified:   notebooks/dressage_rc0/use_dressage_rc0.sql

--- notebooks/dressage_rc0/use_dressage_rc0.sql
+++ notebooks/dressage_rc0/use_dressage_rc0.sql
@@ -9,3 +9,23 @@ USE `dressage_rc0`;
 */
 
 SHOW TABLES;
+
+/*markdown
+## They also follow instructions for cleaning up the entire database if and when needed
+*/
+
+SET FOREIGN_KEY_CHECKS = 0;
+SET @tables = NULL;
+SET GROUP_CONCAT_MAX_LEN = 32768;
+SELECT GROUP_CONCAT('`', table_schema, '`.`', table_name, '`') INTO @tables
+FROM information_schema.tables
+WHERE table_schema = (
+        SELECT DATABASE()
+    );
+SELECT IFNULL(@tables, '') INTO @tables;
+SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
+PREPARE cleaning_statement
+FROM @tables;
+EXECUTE cleaning_statement;
+DEALLOCATE PREPARE cleaning_statement;
+SET FOREIGN_KEY_CHECKS = 1;
