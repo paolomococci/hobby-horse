73c60bb71c6d9f8044fed5bf1747eea4c1896e86
paolomococci <paolomococci@gmail.com>
2023-04-10 00:33:44 +0200

    modified:   notebooks/dressage_rc0/create_tables.sql

--- notebooks/dressage_rc0/create_tables.sql
+++ notebooks/dressage_rc0/create_tables.sql
@@ -1,5 +1,15 @@
+/*markdown
 # create tables
+*/
 
+/*markdown
 ## create table projects
+*/
 
 CREATE TABLE IF NOT EXISTS `projects`;
+
+/*markdown
+## create table employees
+*/
+
+CREATE TABLE IF NOT EXISTS `employees`;
