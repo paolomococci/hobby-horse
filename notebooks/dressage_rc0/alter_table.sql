9ba0823e1f9c29531c024d697b1d64c63634c558
paolomococci <paolomococci@gmail.com>
2023-04-10 10:01:29 +0200

    modified:   notebooks/dressage_rc0/alter_table.sql

--- notebooks/dressage_rc0/alter_table.sql
+++ notebooks/dressage_rc0/alter_table.sql
@@ -19,3 +19,29 @@ USE `dressage_rc0`;
 */
 
 SHOW TABLES;
+
+
+/*markdown
+## add a foreign key to the table employees
+*/
+
+DESCRIBE `employees`;
+
+/*markdown
+### first I add column project
+*/
+
+ALTER TABLE `employees` 
+    ADD COLUMN `project_id` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 
+    AFTER `surname`;
+
+/*markdown
+### after that i add the foreign key fk_project_id
+*/
+
+SET FOREIGN_KEY_CHECKS = OFF;
+ALTER TABLE `employees` 
+    ADD CONSTRAINT `fk_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) 
+    ON UPDATE CASCADE 
+    ON DELETE SET DEFAULT;
+SET FOREIGN_KEY_CHECKS = ON;
