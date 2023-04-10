88730543252f7981baa2cd52fe9788dbd893cfac
paolomococci <paolomococci@gmail.com>
2023-04-10 08:00:50 +0200

    modified:   notebooks/dressage_rc0/template.sql

---
+++ notebooks/dressage_rc0/template.sql
@@ -0,0 +1,21 @@
+/*markdown
+# template
+*/
+
+/*markdown
+## show databases
+*/
+
+SHOW DATABASES;
+
+/*markdown
+## use dressage_rc0
+*/
+
+USE `dressage_rc0`;
+
+/*markdown
+## show tables of dressage_rc0
+*/
+
+SHOW TABLES;
