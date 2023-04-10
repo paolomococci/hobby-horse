1edffa3ccdd213b7087e90a7e0c822fa38abf857
paolomococci <paolomococci@gmail.com>
2023-04-10 10:26:45 +0200

    new file:   notebooks/dressage_rc0/truncate_table.sql

---
+++ notebooks/dressage_rc0/truncate_table.sql
@@ -0,0 +1,27 @@
+/*markdown
+# truncate table statement
+*/
+
+/*markdown
+## show databases
+*/
+
+SHOW DATABASES;
+
+/*markdown
+## use dressage_rc0
+*/
+
+USE `dressage_rc0`;
+
+/*markdown
+## show tables of dressage_rc0
+*/
+
+SHOW TABLES;
+
+/*markdown
+## truncate table employees
+*/
+
+TRUNCATE TABLE `employees`;;
