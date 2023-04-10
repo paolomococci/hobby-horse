c09bb345f09b886dba42809506628c3e294e1dd9
paolomococci <paolomococci@gmail.com>
2023-04-10 22:34:48 +0200

    new file:   notebooks/dressage_rc0/select.sql

---
+++ notebooks/dressage_rc0/select.sql
@@ -0,0 +1,21 @@
+/*markdown
+# select statement
+*/
+
+/*markdown
+## show databases
+*/
+
+SHOW DATABASES;
+
+/*markdown
+## use dressage_rc0
+*/
+
+USE `dressage_rc0`;
+
+/*markdown
+## show tables of dressage_rc0
+*/
+
+SHOW TABLES;
