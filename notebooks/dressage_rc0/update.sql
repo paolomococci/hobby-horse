48ab751a2f33892f36d02bcc5515a11b9d7e6e07
paolomococci <paolomococci@gmail.com>
2023-04-10 23:00:29 +0200

    new file:   notebooks/dressage_rc0/update.sql

---
+++ notebooks/dressage_rc0/update.sql
@@ -0,0 +1,28 @@
+/*markdown
+# update statement
+*/
+
+/*markdown
+## show databases
+*/
+
+SHOW DATABASES;
+
+/*markdown
+## use dressage_rc0
+*/
+
+USE `dressage_rc0`;
+
+/*markdown
+## show tables of dressage_rc0
+*/
+
+SHOW TABLES;
+
+
+/*markdown
+## select columns id, name and idle from table projects
+*/
+
+SELECT `id`, `name`, `idle` FROM `projects`;
