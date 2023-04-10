765be4841b4a4a7106f3b9afca2c90ca432e12b1
paolomococci <paolomococci@gmail.com>
2023-04-10 08:06:40 +0200

    modified:   notebooks/dressage_rc0/create_tables.sql

--- notebooks/dressage_rc0/create_tables.sql
+++ notebooks/dressage_rc0/create_tables.sql
@@ -1,6 +1,25 @@
 /*markdown
-# create tables
+# create tables statement
 */
+
+/*markdown
+## show databases
+*/
+
+SHOW DATABASES;
+
+/*markdown
+## use dressage_rc0
+*/
+
+USE `dressage_rc0`;
+
+/*markdown
+## show tables of dressage_rc0
+*/
+
+SHOW TABLES;
+
 
 /*markdown
 ## create table projects
