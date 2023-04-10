04023b36a968f9ef7c9dcecf8a181ff30bc89dfe
paolomococci <paolomococci@gmail.com>
2023-04-10 10:43:59 +0200

    modified:   notebooks/dressage_rc0/create_tables.sql

--- notebooks/dressage_rc0/create_tables.sql
+++ notebooks/dressage_rc0/create_tables.sql
@@ -58,3 +58,16 @@ CREATE TABLE IF NOT EXISTS `employees` (
 */
 
 DESCRIBE `employees`;
+
+
+/*markdown
+## create table customers
+*/
+
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(16) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
