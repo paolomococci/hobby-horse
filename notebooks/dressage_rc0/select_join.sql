0255212029f3fbee8b02ff9977ae9a72f69e9955
paolomococci <paolomococci@gmail.com>
2023-04-11 06:30:03 +0200

    new file:   notebooks/dressage_rc0/select_join.sql

---
+++ notebooks/dressage_rc0/select_join.sql
@@ -0,0 +1,21 @@
+/*markdown
+# select with join clause
+*/
+
+/*markdown
+## show databases
+*/
+
+SHOW DATABASES;
+
+/*markdown
+## use dressage_rc0
+*/
+
+USE `dressage_rc0`;
+
+/*markdown
+## show tables of dressage_rc0
+*/
+
+SHOW TABLES;
